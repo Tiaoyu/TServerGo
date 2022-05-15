1b7ec28c7ef8d0c857d07a253b245678b2daa048
tiaoyu <txydsx@live.com>
2022-05-15 11:41:00 +0800

    update proto

--- pb/generatepb.bat
+++ pb/generatepb.bat
@@ -1,2 +1,3 @@
 D:\devComplier\protobuf-3.18.1\cmake\build\win32\Release\protoc.exe --go_out=. --go_opt=paths=source_relative game.proto error.proto enum.proto
 D:\devComplier\protobuf-3.18.1\cmake\build\win32\Release\protoc.exe -I=. --cpp_out=D:\workspace\cocosspace\tgame\Classes game.proto error.proto enum.proto
+D:\devComplier\protobuf-3.18.1\cmake\build\win32\Release\protoc.exe -I=. --csharp_out=D:\workspace\unityspace\TGameLTS\unity-src\Assets\Scripts\pb --csharp_opt=base_namespace=PB game.proto error.proto enum.proto 
